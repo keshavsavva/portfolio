a86e6b38f35b25b31ff47fb5b323c07a6805afc2
keshavsavva <keshav.s.avva@gmail.com>
2020-05-29 15:47:53 -0400

    Updates

---
+++ precache-manifest.2a8182b04b4796127ab0f8dc4a5605c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bfec24660ca55e4297e7a9795e403fd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "422b4a2e0d5b72a314bc",
+    "url": "/portfolio/static/js/2.a917a2cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.a917a2cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f20484bed0a4284150cc",
+    "url": "/portfolio/static/js/main.f0a657aa.chunk.js"
+  },
+  {
+    "revision": "9de41cfd101902630250",
+    "url": "/portfolio/static/js/runtime-main.cdee8bff.js"
+  },
+  {
+    "revision": "95035723f1218807433a086144608776",
+    "url": "/portfolio/static/media/linkedInIcon.95035723.png"
+  }
+]);
