9ffa424245f62298c44ae3da8cc75d7f85ff552b
keshavsavva <keshav.s.avva@gmail.com>
2020-06-23 11:48:45 -0400

    added tooltips popups

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react'
 import "../../styles/output.css"
+import Tooltip from '@material-ui/core/Tooltip';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+
+const HtmlTooltip = withStyles((theme) => ({
+    tooltip: {
+      backgroundColor: "rgb(42, 46, 40)",
+      color: '#fff',
+      maxWidth: 220,
+      fontSize: theme.typography.pxToRem(15),
+      border: '1px solid #2eb375',
+      textAlign: 'center'
+    },
+  }))(Tooltip);
 export class Banner extends Component {
 
     render() {
         return (
             <div class="col-md-6">
-                <div class="banner">
-                    <a href={this.props.link}>
-                        <img class="portImg" alt = "appicon" src= {this.props.image} />
-                        <div class="text-block">
-                        {this.props.name}
+                
+                    <HtmlTooltip
+                        title={
+                            <React.Fragment>
+                                <Typography color="inherit">{this.props.name}</Typography>
+                                <p>{this.props.description}</p>
+                                <a class="links" href={this.props.link}>
+                                    {"Deployed"}
+                                </a>
+                                <br></br>
+                                <a class="links" href={this.props.GHlink}>
+                                    {"Github"}
+                                </a>
+                            </React.Fragment>
+                        }
+                        placement={this.props.placement}
+                        interactive
+                    >
+                        <div>
+                            <a href={this.props.link}>
+                                <div class="banner">
+                                    <img class="portImg" alt = "appicon" src= {this.props.image} />
+                                    <div class="text-block">
+                                        {this.props.name}
+                                    </div>
+                                </div>
+                            </a>
                         </div>
-                    </a>
-                </div>
+                    </HtmlTooltip>
           </div>
         )
     }
--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -18,16 +18,16 @@ export class Portfolio extends Component {
                 </div>
                 </div>
                 <div class="row">
-                    <Banner link="https://project3-healthcarepro.herokuapp.com/" image={hcPro} name="HealthCare Pro" /> 
-                    <Banner link="https://limitless-chamber-20765.herokuapp.com/landing.html" image={qStream} name="QuaranStream" />
+                    <Banner link="https://project3-healthcarepro.herokuapp.com/" image={hcPro} name="HealthCare Pro" placement="left-start" GHlink="https://github.com/keshavsavva/Healthcare-Pro" description="A role-based application that helps doctors and patients with the management of healthcare."/> 
+                    <Banner link="https://limitless-chamber-20765.herokuapp.com/landing.html" image={qStream} name="QuaranStream" placement="right-start" GHlink="https://github.com/keshavsavva/QuaranStream" description="QuaranStream: an app for connecting you with content based on your favorite media types and genres"/>
                 </div>
                 <div class="row">
-                    <Banner link="https://keshavsavva.github.io/EventHub/" image={eHub} name="EventHub" />
-                    <Banner link="https://keshavsavva.github.io/Homework5/" image={workDay}name="Work Day Scheduler" />
+                    <Banner link="https://keshavsavva.github.io/EventHub/" image={eHub} name="EventHub" placement="left-start" GHlink="https://github.com/keshavsavva/EventHub" description="EventHub App for finding Public Events based on your location"/>
+                    <Banner link="https://keshavsavva.github.io/Work-Day-Scheduler/" image={workDay}name="Work Day Scheduler" placement="right-start" GHlink="https://github.com/keshavsavva/Work-Day-Scheduler" description="An application for scheduling you Work Day."/>
                 </div>
                 <div class="row">
-                    <Banner link="https://keshavsavva.github.io/Homework4/" image={codingQuiz} name="Coding Quiz" />
-                    <Banner link="https://keshavsavva.github.io/Homework3/" image={passGen} name="Password Generator" />
+                    <Banner link="https://keshavsavva.github.io/Coding-Quiz/" image={codingQuiz} name="Coding Quiz" placement="left-start" GHlink="https://github.com/keshavsavva/Coding-Quiz" description="Test you coding skills here!"/>
+                    <Banner link="https://keshavsavva.github.io/Password-Generator/" image={passGen} name="Password Generator" placement="right-start" GHlink="https://github.com/keshavsavva/Password-Generator" description="Generates a random password based off of your required inputs"/>
                 </div>
             </div>
         )
